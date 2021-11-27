08c6856e92ec446681862cc030190a82a3346983
Yasuhiro Yoshida <892791+YasuhiroYoshida@users.noreply.github.com>
2021-11-27 14:10:25 +0200

    Initial Commit

---
+++ FoodTaskerMobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTaskerMobile
+//
+//  Created by Yasuhiro Yoshida on 2021-11-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
