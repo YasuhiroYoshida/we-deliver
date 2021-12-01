676d2fe07a50f03ec18a66069d3d3c7b817dcb7d
Yasuhiro Yoshida <892791+YasuhiroYoshida@users.noreply.github.com>
2021-12-01 23:04:34 +0200

    asdfasdf

---
+++ FoodTaskerMobile/DeliveryViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DeliveryViewController.swift
+//  FoodTaskerMobile
+//
+//  Created by Yasuhiro Yoshida on 2021-12-01.
+//
+
+import UIKit
+
+class DeliveryViewController: UIViewController {
+  // MARK: - IBOutlet
+  @IBOutlet weak var menuBarButton: UIBarButtonItem!
+
+  // MARK: - View life cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    if self.revealViewController() != nil {
+      menuBarButton.target = self.revealViewController()
+      menuBarButton.action = #selector(SWRevealViewController.revealToggle(_:))
+      self.view.addGestureRecognizer(revealViewController().panGestureRecognizer())
+
+    }
+  }
+}
