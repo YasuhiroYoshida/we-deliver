1f93d1760ef57b4dbb7f3a7cca570f726a316b39
Yasuhiro Yoshida <892791+YasuhiroYoshida@users.noreply.github.com>
2021-12-14 13:42:34 +0200

    Profile half way done

--- FoodTaskerMobile/Controllers/Driver/ProfileViewController.swift
+++ FoodTaskerMobile/Controllers/Driver/ProfileViewController.swift
@@ -6,17 +6,31 @@
 //
 
 import UIKit
+import DropDown
 
 class ProfileViewController: UIViewController {
+  // MARK: - Vars
+  let carDropdown = DropDown()
+  let cars = ["Mazda": "car_1", "Honda": "car_2", "Chevrolet": "car_3"]
 
   // MARK: - IBOutlets
   @IBOutlet weak var menuBarButtonItem: UIBarButtonItem!
   @IBOutlet weak var avatarImageView: UIImageView!
   @IBOutlet weak var usernameLabel: UILabel!
+  @IBOutlet weak var carImageView: UIImageView!
+  @IBOutlet weak var carDropdownButton: UIButton!
+  @IBOutlet weak var updateProfileButton: UIButton!
+  @IBOutlet weak var plateNumberTextfield: UITextField!
+
   // MARK: - Lifecycles
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    loadDriver()
+    loadCarsOntoDropdown()
+  }
+
+  private func loadDriver() {
     if revealViewController() != nil {
       menuBarButtonItem.target = revealViewController()
       menuBarButtonItem.action = #selector(revealViewController().revealToggle(_:))
@@ -33,6 +47,23 @@ class ProfileViewController: UIViewController {
       avatarImageView.clipsToBounds = true
     }
     usernameLabel.text = User.current.name
+  }
+
+  private func loadCarsOntoDropdown() {
+    carDropdown.anchorView = carDropdownButton
+    carDropdown.dataSource = cars.keys.map { $0 }
+    carDropdown.selectionAction = { [unowned self] (index: Int, carName: String) in
+      carDropdownButton.setTitle(carName, for: .normal)
+      carImageView.image = UIImage(named: cars[carName]!)
+    }
+  }
+
+  // MARK: - IBActions
+  @IBAction func selectCarButtonPressed(_ sender: Any) {
+    carDropdown.show()
+  }
+
+  @IBAction func updateProfileButtonPressed(_ sender: Any) {
   }
 
   // MARK: - Navigation
